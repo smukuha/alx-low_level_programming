fd63fd156e4af4abb67a0f31a49a6cec24b76d64
Mukuha Safania <mukuhasafania@gmail.com>
2023-03-06 05:57:31 -0800

    function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,22 @@
-void print_chessboard(char (*a)[8]) {
-    int i, j;
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            if ((i + j) % 2 == 0) {
-                _putchar('B');
-            } else {
-                _putchar('W');
-            }
-        }
-        _putchar('\n');
-    }
+#include <stdio.h>
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: input matrix
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
